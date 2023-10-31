aab6c919b4e4bf57675abb8d4928275ec602b8b0
Oleksander <123253746+Alex-diver@users.noreply.github.com>
2023-10-31 20:44:44 +0200

    hw-2-2

--- models/contacts.js
+++ models/contacts.js
@@ -2,12 +2,12 @@ const fs = require("fs/promises");
 const path = require("path");
 const { nanoid } = require("nanoid");
 
-const contactsPath = path.join(__dirname, "contacts.json");
-
 const listContacts = async () => {
   const data = await fs.readFile(contactsPath);
   return JSON.parse(data);
 };
+
+const contactsPath = path.join(__dirname, "contacts.json");
 
 const getContactById = async (contactId) => {
   const contacts = await listContacts();
@@ -26,11 +26,11 @@ const removeContact = async (contactId) => {
   return result;
 };
 
-const addContact = async (data) => {
+const addContact = async (body) => {
   const contacts = await listContacts();
   const newContact = {
     id: nanoid(),
-    ...data,
+    ...body,
   };
   contacts.push(newContact);
   await fs.writeFile(contactsPath, JSON.stringify(contacts, null, 2));
