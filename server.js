b677cd3c271a7e25cf26f59a9dea3e577f0df5fc
Oleksander <123253746+Alex-diver@users.noreply.github.com>
2023-11-12 19:32:56 +0200

    03-mongodb

--- server.js
+++ server.js
@@ -1,11 +1,8 @@
-// require("dotenv").config();
 const mongoose = require("mongoose");
 
 const app = require("./app");
 
 const { DB_URI, PORT = 3000 } = process.env;
-
-// mongoose.set("strictQuery", true);
 
 mongoose
   .connect(DB_URI)
